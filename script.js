3c320135543bb98a7ee11e00147c9f455b422a7d
Dagim-Daniel <dagimdan19@gmail.com>
2026-08-13 18:53:46 +0100

    level 0- 100 completed

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
-//You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
 
-function makePageForEpisodes(episodeList) {
+  const card = allEpisodes.map(makePageForEpisodes);
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.append(...card);
+  //makePageForEpisodes(allEpisodes);
 }
 
+function makePageForEpisodes(episode) {
+  const filmCard = document
+
+    .getElementById("episode-template")
+
+    .content.cloneNode(true);
+
+  const seasonNumber = episode.season.toString().padStart(2, "0");
+
+  const episodeNumber = episode.number.toString().padStart(2, "0");
+
+  const episodeCode = `S${seasonNumber}E${episodeNumber}`;
+
+  filmCard.querySelector(".episode-title").textContent =
+    `${episode.name}: ${episodeCode}`;
+
+  filmCard.querySelector(".episode-image").src = episode.image.medium;
+
+  filmCard.querySelector(".episode-image").alt = episode.name;
+
+  filmCard.querySelector(".episode-summary").innerHTML = episode.summary;
+
+  return filmCard;
+}
 window.onload = setup;
